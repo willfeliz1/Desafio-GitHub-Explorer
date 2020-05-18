1d81616223e653adcf6ae03bb91368f02accf314
willfeliz1 <william_mfelizardo@hotmail.com>
2020-05-18 15:06:26 -0300

    detail repository page and applying CSS

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +12,56 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="RocketSeat"
+          />
+          <div>
+            <strong>rockectseat/unform</strong>
+            <p>descricao do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>48</strong>
+            <span>forks</span>
+          </li>
+
+          <li>
+            <strong>67</strong>
+            <span>Issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="asdasd">
+          <div>
+            <strong>asdasd</strong>
+            <p>asdasd</p>
+          </div>
+          <FiChevronLeft size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
